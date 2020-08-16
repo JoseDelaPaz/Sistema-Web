26ba6b2273aa12eedfde6f052992300b9eca5803
Jose <josedelapazbeltre@gmail.com>
2020-08-16 17:56:47 -0400

    Usuario por defecto

---
+++ Data/IserviceLogin.cs
@@ -0,0 +1,38 @@
+﻿using CapaModelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class IserviceLogin
+    {
+        List<EmpleadoM> empleadoMs;
+        public IserviceLogin()
+        {
+            empleadoMs = new List<EmpleadoM>();
+
+            empleadoMs.Add(new EmpleadoM
+            {
+                ID = 100,
+                Nombre = "Juan",
+                Apellido = "Vargas",
+                Sexo = "Masculino",
+                Telefono = "8094325432",
+                Direccion = "Azua",
+                marca_vehiculo = "Toyota",
+                Correo = "admin@gmail.com",
+                Contrasena = "12345",
+                Roles = "Administrador"
+                
+            });
+        }
+        public EmpleadoM GetUsuario(string login, string password)
+        {
+            return empleadoMs.FirstOrDefault(u => u.Correo == login && u.Contrasena == password);
+        }
+
+
+    }
+}
--- TallerMecanica/Pages/IniciarSesion.cshtml.cs
+++ TallerMecanica/Pages/IniciarSesion.cshtml.cs
@@ -20,55 +20,82 @@ namespace TallerMecanica.Pages
     [AllowAnonymous]
     public class IniciarSesionModel : PageModel
     {
-        private readonly ApplicationDbContext _db; 
+        private readonly ApplicationDbContext _db;
         public IniciarSesionModel(ApplicationDbContext db)
         {
             _db = db;
-        }
-        [BindProperty]
-        public LoginModel loginModel { get; set; }
+        } 
 
         [BindProperty]
+        public LoginModel loginModel { get; set; }
         public List<EmpleadoM> empleadoMs { get; set; }
         public void OnGet()
         {
+
         }
-
-
         public ActionResult Onpost(string returnUrl)
         {
-
+            #region usuario Login por Base de datos
             var usuario = GetEmpleadoM(loginModel.Usuario, loginModel.Password);
-
             if (usuario != null)
             {
                 var claims = new List<Claim>()
-                {   
-                    new Claim ("id", usuario.ID.ToString()),    
+                {
+                    new Claim ("id", usuario.ID.ToString()),
                     new Claim(ClaimTypes.Name, usuario.Nombre),
                     new Claim(ClaimTypes.Role, usuario.Roles)
                 };
 
                 var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                 var principal = new ClaimsPrincipal(identity);
-                
-               HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                    principal,
-                    new AuthenticationProperties
-                    {
-                        IsPersistent = loginModel.Recordarme
-                    }).GetAwaiter().GetResult();
-               
+
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                     principal,
+                     new AuthenticationProperties
+                     {
+                         IsPersistent = loginModel.Recordarme
+
+                     }).GetAwaiter().GetResult();
+
                 returnUrl = returnUrl == null || returnUrl == "/" ? "Index" : returnUrl;
                 return RedirectToPage(returnUrl);
             }
+            #endregion
+
+
+
+            #region Usuario administrador por defecto
+            /// Usuario administrador por defecto
+            IserviceLogin iservice = new IserviceLogin();
+                var aux = iservice.GetUsuario(loginModel.Usuario, loginModel.Password);
+                if (aux != null)
+                {
+                    var claims = new List<Claim>()
+                {
+                new Claim ("id", aux.ID.ToString()),
+                new Claim (ClaimTypes.Name, aux.Nombre),
+                new Claim (ClaimTypes.Role, aux.Roles)
+                };
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        principal,
+                        new AuthenticationProperties
+                        {
+                            IsPersistent = loginModel.Recordarme
+                        }
+                        ).GetAwaiter().GetResult();
+                    returnUrl = returnUrl == null || returnUrl == "/" ? "Index" : returnUrl;
+                    return RedirectToPage(returnUrl);
+                }
+                #endregion
 
             return Page();
         }
 
         public EmpleadoM GetEmpleadoM(string login, string password)
         {
-
             empleadoMs = _db.empleado.ToList();
             return empleadoMs.FirstOrDefault(u => u.Correo == login && u.Contrasena == password);
         }
